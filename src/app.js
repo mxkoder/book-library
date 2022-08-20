5b33f2d749007f3fcc711ece7f92b1e8ff152056
Agnes Beviz <agnes.beviz@gmail.com>
2022-08-20 13:12:31 +0100

    project setup - app successfully listening on port 4000

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+module.exports = app;
+
+
+// const express = require('express');
+// const artistRouter = require('./routes/artist');
+// const albumRouter = require('./routes/album');
+
+// const app = express();
+
+// app.use(express.json());
+
+// app.use('/artist', artistRouter);
+
+// //app.use('/artist/:artistId/album', albumRouter);
+// app.use('/album', albumRouter);
+
+
+// module.exports = app;
