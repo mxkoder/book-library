cc13820f89f0d1a6818d060ac7cde3d4079c69b5
Agnes Beviz <agnes.beviz@gmail.com>
2022-08-20 17:39:04 +0100

    first test running correctly

--- src/models/reader.js
+++ src/models/reader.js
@@ -2,9 +2,8 @@ module.exports = (connection, DataTypes) => {
     const schema = {
         name: DataTypes.STRING,
         email: DataTypes.STRING,
+        };
+    
+        const ReaderModel = connection.define('Reader', schema);
+        return ReaderModel;
     };
-
-    //pass in what we want our table to be called and the schema
-    const ReaderModel = connection.define('Reader', schema);
-    return ReaderModel;
-}
--- tests/reader.test.js
+++ tests/reader.test.js
@@ -3,9 +3,7 @@ const request = require('supertest');
 const { Reader } = require('../src/models');
 const app = require('../src/app');
 
-describe ('/readers', () => {
-
-    // before = same as beforeAll in jest
+describe('/readers', () => {
     before(async () => Reader.sequelize.sync());
 
     beforeEach(async () => {
@@ -14,14 +12,14 @@ describe ('/readers', () => {
 
     describe('with no records in the database', () => {
         describe('POST /readers', () => {
-            it('creates a new reader in the database', async () => {
-                const response = await request(app).post('/readers').send({
-                    name: 'Elizabeth Bennet',
-                    email: 'future_ms_darcy@gmail.com',
-                });
-
-                expect(response.status).to.equal(201);
+        it('creates a new reader in the database', async () => {
+            const response = await request(app).post('/readers').send({
+            name: 'Elizabeth Bennet',
+            email: 'future_ms_darcy@gmail.com',
             });
+
+            expect(response.status).to.equal(201);
+        });
         });
     });
 });
