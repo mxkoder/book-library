dc8f9879cfff477646df458578d48fe9d9eb128c
Agnes Beviz <agnes.beviz@gmail.com>
2022-08-21 17:49:19 +0100

    create new reader - test passing

--- src/controllers/readers.js
+++ src/controllers/readers.js
@@ -2,6 +2,6 @@ const { Reader } = require('../models');
 
 exports.create = async (req, res) => {
     const newReader = await Reader.create(req.body);
-    req.status(201).json(newReader);
+    res.status(201).json(newReader);
 };
 
